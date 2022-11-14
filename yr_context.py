3028682a3cbf0d51d2fcdedc3c4d959204233eb4
Callum Sinclair <c-sinclair@outlook.com>
2022-11-14 22:45:11 +0100

    Gather all data into a list of dicts
    Year, Min, Mean, Max

--- yr_context.py
+++ yr_context.py
@@ -10,14 +10,16 @@ client_id = '<REDACTED>'
 trondheim = 'SN68125'
 
 today = datetime.date.today()
-for years_ago in range(11, 0, -1):
+history_list = list(())
+for years_ago in range(10, -1, -1):
     get_date = datetime.date(today.year - years_ago, today.month, today.day)
+    print(get_date)
 
     # Define endpoint and parameters
     endpoint = 'https://frost.met.no/observations/v0.jsonld'
     parameters = {
         'sources': trondheim,
-        'elements': 'max(air_temperature P1D),min(air_temperature P1D)',
+        'elements': 'max(air_temperature P1D),min(air_temperature P1D),mean(air_temperature P1D)',
         'referencetime': get_date,
         'timeoffsets': 'default',
         'levels': 'default',
@@ -29,6 +31,18 @@ for years_ago in range(11, 0, -1):
 
     try:
         data = json['data']
-        print("{}: {}'C to {}'C".format(get_date, data[0]['observations'][1]['value'], data[0]['observations'][0]['value']))
+        air_temp_dict = {'year': today.year, 'min': False, 'mean': False, 'max': False }
+        for observation in data[0]['observations']:
+            if observation['elementId'].startswith('min'):
+                air_temp_dict['min'] = observation['value']
+            if observation['elementId'].startswith('mean'):
+                air_temp_dict['mean'] = observation['value']
+            if observation['elementId'].startswith('max'):
+                air_temp_dict['max'] = observation['value']
+        history_list.append(air_temp_dict)
+        # print(air_temp_dict)
+
     except KeyError:
-        print(get_date)
+        print("ERROR")
+
+print(history_list)
