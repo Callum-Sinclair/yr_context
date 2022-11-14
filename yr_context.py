11c0c9fbb1ba8ec0c57967a2a13f623e17c092d3
Callum Sinclair <c-sinclair@outlook.com>
2022-11-14 18:52:23 +0100

    get last 6 years of data for day

--- yr_context.py
+++ yr_context.py
@@ -11,37 +11,41 @@ client_id = '<REDACTED>'
 trondheim = 'SN68125'
 
 today = datetime.date.today()
+for years_ago in range(6, 0, -1):
+    get_date = datetime.date(today.year - years_ago, today.month, today.day)
+    print(years_ago)
+    print(get_date)
 
-# Define endpoint and parameters
-endpoint = 'https://frost.met.no/observations/v0.jsonld'
-parameters = {
-    'sources': trondheim,
-    'elements': 'max(air_temperature P1D),min(air_temperature P1D)',
-    'referencetime': today,
-    'timeoffsets': 'default',
-    'levels': 'default',
-}
-# Issue an HTTP GET request
-r = requests.get(endpoint, parameters, auth=(client_id,''))
-# Extract JSON data
-json = r.json()
+    # Define endpoint and parameters
+    endpoint = 'https://frost.met.no/observations/v0.jsonld'
+    parameters = {
+        'sources': trondheim,
+        'elements': 'max(air_temperature P1D),min(air_temperature P1D)',
+        'referencetime': get_date,
+        'timeoffsets': 'default',
+        'levels': 'default',
+    }
+    # Issue an HTTP GET request
+    r = requests.get(endpoint, parameters, auth=(client_id,''))
+    # Extract JSON data
+    json = r.json()
 
-# Check if the request worked, print out any errors
-if r.status_code == 200:
-    data = json['data']
-    print('Data retrieved from frost.met.no!')
-else:
-    print('Error! Returned status code %s' % r.status_code)
-    print('Message: %s' % json['error']['message'])
-    print('Reason: %s' % json['error']['reason'])
+    # Check if the request worked, print out any errors
+    if r.status_code == 200:
+        data = json['data']
+        print('Data retrieved from frost.met.no!')
+    else:
+        print('Error! Returned status code %s' % r.status_code)
+        print('Message: %s' % json['error']['message'])
+        print('Reason: %s' % json['error']['reason'])
 
-# This will return a Dataframe with all of the observations in a table format
-df = pd.DataFrame()
-for i in range(len(data)):
-    row = pd.DataFrame(data[i]['observations'])
-    row['referenceTime'] = data[i]['referenceTime']
-    row['sourceId'] = data[i]['sourceId']
-    df = df.append(row)
+    # This will return a Dataframe with all of the observations in a table format
+    df = pd.DataFrame()
+    for i in range(len(data)):
+        row = pd.DataFrame(data[i]['observations'])
+        row['referenceTime'] = data[i]['referenceTime']
+        row['sourceId'] = data[i]['sourceId']
+        df = df.append(row)
 
-df = df.reset_index()
-print(df.head())
+    df = df.reset_index()
+    print(df.head())
