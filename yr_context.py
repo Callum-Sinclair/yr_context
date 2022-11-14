25d38563bba7e546c4c5f7b2709397f78d56ac93
Callum Sinclair <c-sinclair@outlook.com>
2022-11-14 19:14:52 +0100

    print last decade of max/min

--- yr_context.py
+++ yr_context.py
@@ -11,10 +11,8 @@ client_id = '<REDACTED>'
 trondheim = 'SN68125'
 
 today = datetime.date.today()
-for years_ago in range(6, 0, -1):
+for years_ago in range(11, 0, -1):
     get_date = datetime.date(today.year - years_ago, today.month, today.day)
-    print(years_ago)
-    print(get_date)
 
     # Define endpoint and parameters
     endpoint = 'https://frost.met.no/observations/v0.jsonld'
@@ -31,21 +29,19 @@ for years_ago in range(6, 0, -1):
     json = r.json()
 
     # Check if the request worked, print out any errors
-    if r.status_code == 200:
+    try:
         data = json['data']
-        print('Data retrieved from frost.met.no!')
-    else:
-        print('Error! Returned status code %s' % r.status_code)
-        print('Message: %s' % json['error']['message'])
-        print('Reason: %s' % json['error']['reason'])
+        print("{}: {}'C to {}'C".format(get_date, data[0]['observations'][1]['value'], data[0]['observations'][0]['value']))
+    except KeyError:
+        print(get_date)
 
     # This will return a Dataframe with all of the observations in a table format
-    df = pd.DataFrame()
-    for i in range(len(data)):
-        row = pd.DataFrame(data[i]['observations'])
-        row['referenceTime'] = data[i]['referenceTime']
-        row['sourceId'] = data[i]['sourceId']
-        df = df.append(row)
-
-    df = df.reset_index()
-    print(df.head())
+    # df = pd.DataFrame()
+    # for i in range(len(data)):
+    #     row = pd.DataFrame(data[i]['observations'])
+    #     row['referenceTime'] = data[i]['referenceTime']
+    #     row['sourceId'] = data[i]['sourceId']
+    #     df = df.append(row)
+    #
+    # df = df.reset_index()
+    # print(df.head())
