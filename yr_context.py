ab7adee35bc1af53f973e8f7cf34301a3b60892d
Callum Sinclair <c-sinclair@outlook.com>
2022-11-14 18:32:34 +0100

    print data from current date

--- yr_context.py
+++ yr_context.py
@@ -1,6 +1,7 @@
 # Libraries needed (pandas is not standard and must be installed in Python)
 import requests
 import pandas as pd
+import datetime
 
 # Insert your own client ID here
 # Initially hard-coded, removed to publish, replaced with external input in later commit
@@ -9,12 +10,16 @@ client_id = '<REDACTED>'
 # Source
 trondheim = 'SN68125'
 
+today = datetime.date.today()
+
 # Define endpoint and parameters
 endpoint = 'https://frost.met.no/observations/v0.jsonld'
 parameters = {
     'sources': trondheim,
     'elements': 'max(air_temperature P1D),min(air_temperature P1D)',
-    'referencetime': '2020-04-01',
+    'referencetime': today,
+    'timeoffsets': 'default',
+    'levels': 'default',
 }
 # Issue an HTTP GET request
 r = requests.get(endpoint, parameters, auth=(client_id,''))
