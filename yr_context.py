6a25f479f335c959c7ec582e6908269b8b0e1d96
Callum Sinclair <c-sinclair@outlook.com>
2022-11-14 19:23:48 +0100

    get rid of junk

--- yr_context.py
+++ yr_context.py
@@ -1,6 +1,5 @@
 # Libraries needed (pandas is not standard and must be installed in Python)
 import requests
-import pandas as pd
 import datetime
 
 # Insert your own client ID here
@@ -28,20 +27,8 @@ for years_ago in range(11, 0, -1):
     # Extract JSON data
     json = r.json()
 
-    # Check if the request worked, print out any errors
     try:
         data = json['data']
         print("{}: {}'C to {}'C".format(get_date, data[0]['observations'][1]['value'], data[0]['observations'][0]['value']))
     except KeyError:
         print(get_date)
-
-    # This will return a Dataframe with all of the observations in a table format
-    # df = pd.DataFrame()
-    # for i in range(len(data)):
-    #     row = pd.DataFrame(data[i]['observations'])
-    #     row['referenceTime'] = data[i]['referenceTime']
-    #     row['sourceId'] = data[i]['sourceId']
-    #     df = df.append(row)
-    #
-    # df = df.reset_index()
-    # print(df.head())
