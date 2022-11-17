ec01465ed50223c57824055d3cb9d7eb001d6cb6
Callum Sinclair <c-sinclair@outlook.com>
2022-11-17 22:57:28 +0100

    enable getting multiple years worth of data
    Could do with a code clean up soon...

--- yr_context.py
+++ yr_context.py
@@ -25,6 +25,7 @@ parser.add_argument("-d", "--day", default=0, help="day of month")
 parser.add_argument("-m", "--month", default=0, help="number of month")
 parser.add_argument("-l", "--location", default='Trondheim', help="Location for temparatures, ignored if location_id given", choices=sources.keys())
 parser.add_argument("-i", "--location_id", default=0, help="Weather station ID to use, overrides --location")
+parser.add_argument("-n", "--num_days", default=1, help="The number of days to fetch for each year (requested day and (n-1) days previous)")
 
 args = parser.parse_args()
 config = vars(args)
@@ -52,70 +53,78 @@ except ValueError:
     print('Invalid date requested')
     raise
 
-print(requested_date)
-
-print('Fetching data for...')
-history_list = list(())
-for years_ago in range(10, -1, -1):
-    get_date = datetime.date(requested_date.year - years_ago, requested_date.month, requested_date.day)
-    print(get_date)
-
-    # Define endpoint and parameters
-    endpoint = 'https://frost.met.no/observations/v0.jsonld'
-    parameters = {
-        'sources': location,
-        'elements': 'max(air_temperature P1D),min(air_temperature P1D),mean(air_temperature P1D)',
-        'referencetime': get_date,
-        'timeoffsets': 'default',
-        'levels': 'default',
-    }
-    # Issue an HTTP GET request
-    r = requests.get(endpoint, parameters, auth=(client_id,''))
-    # Extract JSON data
-    json = r.json()
-
-    try:
-        data = json['data']
-        air_temp_dict = {'year': get_date.year, 'min': False, 'mean': False, 'max': False }
-        for observation in data[0]['observations']:
-            if observation['elementId'].startswith('min'):
-                air_temp_dict['min'] = observation['value']
-            if observation['elementId'].startswith('mean'):
-                air_temp_dict['mean'] = observation['value']
-            if observation['elementId'].startswith('max'):
-                air_temp_dict['max'] = observation['value']
-        history_list.append(air_temp_dict)
-        # print(air_temp_dict)
-
-    except KeyError:
-        print("ERROR")
-
-print(history_list)
-
-# reformat data for day graph
-mean = list(())
-mean_year = list(())
-min = list(())
-min_year = list(())
-max = list(())
-max_year = list(())
-for year_data in history_list:
-    if year_data['mean']:
-        mean.append(year_data['mean'])
-        mean_year.append(year_data['year'])
-    if year_data['min']:
-        min.append(year_data['min'])
-        min_year.append(year_data['year'])
-    if year_data['max']:
-        max.append(year_data['max'])
-        max_year.append(year_data['year'])
-
 seaborn.set_theme(style="dark")
 seaborn.set()
-plt.plot(mean_year, mean, 'go')
-plt.plot(min_year, min, 'bo')
-plt.plot(max_year, max, 'ro')
+
+print('Requesting data from {}'.format(requested_date))
+print('Fetching data for...')
+
+for days_back in range(0, int(config['num_days'])):
+    history_list = list(())
+    for years_ago in range(10, -1, -1):
+        get_date = datetime.date(requested_date.year - years_ago, requested_date.month, requested_date.day) - datetime.timedelta(days=days_back)
+        print(get_date)
+
+        # Define endpoint and parameters
+        endpoint = 'https://frost.met.no/observations/v0.jsonld'
+        parameters = {
+            'sources': location,
+            'elements': 'max(air_temperature P1D),min(air_temperature P1D),mean(air_temperature P1D)',
+            'referencetime': get_date,
+            'timeoffsets': 'default',
+            'levels': 'default',
+        }
+        # Issue an HTTP GET request
+        r = requests.get(endpoint, parameters, auth=(client_id,''))
+        # Extract JSON data
+        json = r.json()
+
+        try:
+            data = json['data']
+            air_temp_dict = {'year': get_date.year, 'min': False, 'mean': False, 'max': False }
+            for observation in data[0]['observations']:
+                if observation['elementId'].startswith('min'):
+                    air_temp_dict['min'] = observation['value']
+                if observation['elementId'].startswith('mean'):
+                    air_temp_dict['mean'] = observation['value']
+                if observation['elementId'].startswith('max'):
+                    air_temp_dict['max'] = observation['value']
+            history_list.append(air_temp_dict)
+            # print(air_temp_dict)
+
+        except KeyError:
+            print("ERROR")
+
+    # print(history_list)
+
+    # reformat data for day graph
+    mean = list(())
+    mean_year = list(())
+    min = list(())
+    min_year = list(())
+    max = list(())
+    max_year = list(())
+    for year_data in history_list:
+        if year_data['mean']:
+            mean.append(year_data['mean'])
+            mean_year.append(year_data['year'])
+        if year_data['min']:
+            min.append(year_data['min'])
+            min_year.append(year_data['year'])
+        if year_data['max']:
+            max.append(year_data['max'])
+            max_year.append(year_data['year'])
+
+    plt.plot(mean_year, mean, 'go')
+    plt.plot(min_year, min, 'bo')
+    plt.plot(max_year, max, 'ro')
+
+
 plt.axhline(y=0, color='b', linestyle='-')
-plt.title('Temperature in {} on {}/{} by Year'.format(location_name, requested_date.day, requested_date.month))
+if int(config['num_days']) == 1:
+    plt.title('Temperature in {} on {}/{} by Year'.format(location_name, requested_date.day, requested_date.month))
+else:
+    start_date = datetime.date(requested_date.year - years_ago, requested_date.month, requested_date.day) - datetime.timedelta(days=int(config['num_days']))
+    plt.title('Temperature in {} on {}/{} to {}/{} by Year'.format(location_name, start_date.day, start_date.month, requested_date.day, requested_date.month))
 plt.plot()
 plt.show()
